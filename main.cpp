c756a3e6f8d8f39c82d46addf52e90213224cbb9
Marhtini <JohnMartinez90@gmail.com>
2024-01-26 13:08:09 -0500

    He runnin'

--- main.cpp
+++ main.cpp
@@ -9,8 +9,27 @@ int main(){
     Texture2D map = LoadTexture("nature_tileset/worldmap.png");
     float mapX{0};
     Vector2 mapPos{mapX, 0.0};
-
+    
+    // Movement speed (scale)
     float speed{4.0};
+
+    Texture2D knight{};
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
+
+    Vector2 knightPos{
+        (float)WINDOW_DIM[0]/2.0f - 4.0f * (0.5f * (float)knight_idle.width/6.0f),
+        (float)WINDOW_DIM[1]/2.0f - 4.0f * (0.5f * (float)knight_idle.height)
+    };
+
+    // 1: facing right, -1: facing left.
+    float rightLeft{1.f};
+
+    // Animation variables
+    float runningTime{0};
+    int frame{0};
+    const int MAXFRAMES{6};
+    const float UPDATETIME{1.f/12.f};
 
     SetTargetFPS(60);
 
@@ -27,10 +46,31 @@ int main(){
         if (Vector2Length(direction) != 0.0){
             // set mapPos = mapPos - direction
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            // check if left or right
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            knight = knight_run;
+
+        }
+        else{
+            knight = knight_idle;
         }
 
+        // Draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
-       
+
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= UPDATETIME){
+            frame++;
+            runningTime = 0.f;
+            if (frame > MAXFRAMES) frame = 0;
+        }
+
+        // Draw the character
+        Rectangle source{frame * (float)knight.width / 6.f, 0.f, rightLeft * (float)knight.width / 6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width / 6.0f, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+
         EndDrawing();
     }
 
