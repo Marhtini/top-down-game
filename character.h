66259a8ffbbaf01d385b763c5bbb6232ce5c906f
Marhtini <JohnMartinez90@gmail.com>
2024-01-26 14:20:09 -0500

    created header files and seperated character class.

---
+++ character.h
@@ -0,0 +1,27 @@
+#include <raylib.h>
+
+class Character
+{
+public:
+    Character();
+    Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
+
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    // 1: facing right, -1: facing left.
+    float rightLeft{1.f};
+    // Animation variables
+    float runningTime{0};
+    int frame{0};
+    int maxFrames{6};
+    float updatetime{1.f / 12.f};
+    float speed{4.f};
+    float width{};
+    float height{};
+};
